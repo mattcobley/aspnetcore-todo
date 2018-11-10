fee375b83909510a22c51a3b6acef444d6443883
Matthew Cobley <mattcobley@outlook.com>
2018-11-10 19:58:55 +0000

    Correct items as virtual property of list

---
+++ Migrations/20181110191046_linklistitemstolistscorrectly.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace aspnetcoretodo.Migrations
+{
+    public partial class linklistitemstolistscorrectly : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
